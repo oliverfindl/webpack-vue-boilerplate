a6c32c98e25ffdc4ad6fa4a9ab49878c344c43dc
Oliver Findl <github@findl.sk>
2018-06-02 12:33:00 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # webpack-vue-boilerplate
-Simple boilerplate for building Vue app with Webpack.
+
+![license](https://img.shields.io/github/license/oliverfindl/webpack-vue-boilerplate.svg?style=flat)
+[![paypal](https://img.shields.io/badge/donate-paypal-blue.svg?colorB=0070ba&style=flat)](https://paypal.me/oliverfindl)
+
+Simple boilerplate for building [Vue](https://github.com/vuejs/vue) app with [Webpack](https://github.com/webpack/webpack).
+
+---
+
+## Install
+
+```bash
+# Clone repository from GitHub to <folder-name>
+$ git clone https://github.com/oliverfindl/webpack-vue-boilerplate <folder-name>
+
+# Switch to <folder-name>
+$ cd <folder-name>
+
+# Install all dependencies
+$ npm install
+
+# [optional] Update all dependencies
+$ npm update
+```
+
+## Usage
+
+```bash
+# Start Webpack development server with Hot Module Replacement enabled
+$ npm run dev
+
+# Build and minify Vue app
+$ npm run build
+```
+
+## App structure
+
+```
+.                       # root
+├── dist                # folder for distributables 
+├── src                 # folder for source code
+│   ├── assets          # folder for assets (images, video, audio, fonts, etc.)
+│   ├── App.vue         # main Vue component file
+│   ├── index.html      # index file for Vue app
+│   └── main.js         # main Vue app file
+├── package.json        # configuration file for npm
+└── webpack.config.js   # configuration file for Webpack
+```
+
+---
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT)
---
+++ webpack.config.js
@@ -0,0 +1,103 @@
+"use strict";
+
+const path = require("path");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	mode: process.env.NODE_ENV,
+	target: "web",
+	entry: path.resolve(__dirname, "src/main.js"),
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "[name].js"
+	},
+	devServer: {
+		historyApiFallback: true,
+		overlay: {
+			warnings: true,
+			errors: true
+		}
+	},
+	performance: {
+		hints: false
+	},
+	module: {
+		rules: [{
+			test: /\.vue$/,
+			loader: "vue-loader"
+		}, {
+			test: /\.js$/,
+			loader: "babel-loader",
+			exclude: file => /node_modules/.test(file) && !/\.vue\.js/.test(file),
+			options: {
+				presets: [
+					"env",
+					"minify"
+				]
+			}
+		}, {
+			test: /\.css$/,
+			use: [
+				"vue-style-loader",
+				"css-loader"
+			]
+		}, {
+			test: /\.scss$/,
+			use: [
+				"vue-style-loader",
+				"css-loader",
+				"sass-loader"
+			]
+		}, {
+			test: /\.sass$/,
+			use: [
+				"vue-style-loader",
+				"css-loader",
+				"sass-loader?indentedSyntax"
+			]
+		}, {
+			test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+			loader: "file-loader",
+			options: {
+				name: "images/[name].[hash:8].[ext]"
+			}
+		}, {
+			test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+			loader: "file-loader",
+			options: {
+				name: "media/[name].[hash:8].[ext]"
+			}
+		}, {
+			test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+			loader: "file-loader",
+			options: {
+				name:"fonts/[name].[hash:8].[ext]"
+			}
+		}]
+	},
+	plugins: [
+		new VueLoaderPlugin(),
+		new HtmlWebpackPlugin({
+			filename: "index.html",
+			template: "src/index.html",
+			inject: true,
+			favicon: "src/assets/favicon.ico",
+			minify: {
+				collapseInlineTagWhitespace: true,
+				collapseWhitespace: true,
+				html5: true,
+				keepClosingSlash: true,
+				removeComments: true
+			},
+			xhtml: true
+		})
+	],
+	resolve: {
+		extensions: [".js", ".vue", ".json"],
+		alias: {
+			"vue$": "vue/dist/vue.esm.js",
+			"@": path.resolve(__dirname, "src")
+		}
+	}
+}
